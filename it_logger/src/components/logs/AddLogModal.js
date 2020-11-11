ae613ab6320a74f85f53b858cbe42d5d0f5f4caf
vvictorious <vvictorious30@gmail.com>
2020-11-11 15:54:44 -0700

    added some error handling for the modal submit that presents a toast if either message or tech are empty

--- it_logger/src/components/logs/AddLogModal.js
+++ it_logger/src/components/logs/AddLogModal.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react'
 
+import M from 'materialize-css/dist/js/materialize.min.js'
+
 const AddLogModal = () => {
     const [message, setMessage] = useState('')
     const [attention, setAttention] = useState(false)
     const [tech, setTech] = useState('')
 
     const onSubmit = () => {
-        console.log(message, tech, attention)
+        if (message === '' || tech === '') {
+            M.toast({
+                html: 'Please enter a message and tech'
+            })
+        } else {
+            console.log(message, attention, tech)
+        }
     }
 
     return (
