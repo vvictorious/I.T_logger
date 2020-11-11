2b97454c088f260219fb93013f4d71a29c7d5b2e
vvictorious <vvictorious30@gmail.com>
2020-11-11 11:56:16 -0700

    rendering a logitem and added conditional class based on if attention is needed

--- it_logger/src/components/logs/Logs.js
+++ it_logger/src/components/logs/Logs.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import LogItem from './LogItem'
 
 const Logs = () => {
 
@@ -29,7 +30,9 @@ const Logs = () => {
                 <h4 className='center'>System Logs</h4>
             </li>
             {!loading && logs.length === 0 ? (<p className='center'>No Logs To Show....</p>) : 
-            (logs.map(log => <li>{log.message}</li>))}
+            (logs.map(log => 
+                <LogItem log={log} key={log.id} />
+            ))}
         </ul>
     )
 }
