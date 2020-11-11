ddfe9b28174d03a97148c116daa4f5cd4916fac0
vvictorious <vvictorious30@gmail.com>
2020-11-11 12:18:29 -0700

    using moment to format the log date in the log item component

--- it_logger/src/components/logs/LogItem.js
+++ it_logger/src/components/logs/LogItem.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import Moment from 'react-moment'
 import PropTypes from 'prop-types'
 
 const LogItem = ({log}) => {
     return (
         <li className='collection-item'>
             <div>
-                <a href='#edit-log-modal' className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`} >{log.message}</a>
+                <a 
+                    href='#edit-log-modal' 
+                    className={`modal-trigger ${
+                        log.attention ? 'red-text' : 'blue-text'}`
+                }>
+                {log.message}
+                </a>
+                <br/>
+                <span className='grey-text'>
+                    <span className='black-text'>ID#{log.id}</span> Last updated by {' '}
+                    <span className='black-text'>{log.tech}</span> On <Moment format='MMM Do YYYY, h:mm:ss a'>{log.date}</Moment>
+                </span>
             </div>
         </li>
     )
