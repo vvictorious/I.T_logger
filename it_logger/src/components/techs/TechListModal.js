2ba994468d215a73e5783f0e0cfdc37d7682e2ea
vvictorious <vvictorious30@gmail.com>
2020-11-12 11:54:46 -0700

    pretty sure the ui is done now that the techlist modal is rendering the techitem component successfully

--- it_logger/src/components/techs/TechListModal.js
+++ it_logger/src/components/techs/TechListModal.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import TechItem from './TechItem'
 
 const TechListModal = () => {
 
@@ -25,7 +26,7 @@ const TechListModal = () => {
                 <h4>Technician List</h4>
                 <ul className='collection'>
                     {!loading && techs.map(tech => (
-                        <li className='collection-item'>{tech.firstName}</li>
+                        <TechItem tech={tech} key={tech.id}/>
                     ))}
                 </ul>
             </div>
