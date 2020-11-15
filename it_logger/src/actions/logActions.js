f69adf63f26666d0d13e587e59a6dd5e4d73a88a
vvictorious <vvictorious30@gmail.com>
2020-11-15 10:00:40 -0700

    added a search action and reducer for the logs. still need to bring it in to the component

--- it_logger/src/actions/logActions.js
+++ it_logger/src/actions/logActions.js
@@ -1,4 +1,14 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG } from './types'
+import { 
+    GET_LOGS, 
+    SET_LOADING, 
+    LOGS_ERROR, 
+    ADD_LOG, 
+    DELETE_LOG, 
+    SET_CURRENT, 
+    CLEAR_CURRENT, 
+    UPDATE_LOG,
+    SEARCH_LOGS 
+} from './types'
 
 // export const getLogs = () => {
 //     return async (dispatch) => {
@@ -95,6 +105,26 @@ export const updateLog = log => async dispatch => {
         const data = await res.json()
         dispatch({
             type: UPDATE_LOG,
+            payload: data
+        })        
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.responsse.data
+        })
+    }
+}
+
+// search server logs
+export const searchLogs = (text) => async dispatch => {
+    try {
+        setLoading()
+
+        const res = await fetch(`/logs?q=${text}`)
+        const data = await res.json()
+    
+        dispatch({
+            type: SEARCH_LOGS,
             payload: data
         })        
     } catch (err) {
