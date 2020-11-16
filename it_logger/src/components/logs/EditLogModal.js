0ef089332a0fa30cd67d0c1371d987d04b2e3c4f
vvictorious <vvictorious30@gmail.com>
2020-11-16 14:46:39 -0700

    did the same for editLogsModal

--- it_logger/src/components/logs/EditLogModal.js
+++ it_logger/src/components/logs/EditLogModal.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import TechSelectOptions from '../techs/TechSelectOptions'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import M from 'materialize-css/dist/js/materialize.min.js'
@@ -58,9 +59,7 @@ const EditLogModal = ({ updateLog, current }) => {
                             className='browser-default' 
                             onChange={e => setTech(e.target.value)}>
                                 <option value='' disabled>Select Technician</option>
-                                <option value='John Doe'>John Doe</option>
-                                <option value='Sam Smith'>Sam Smith</option>
-                                <option value='Sarah Wilson'>Sarah Wilson</option>
+                                <TechSelectOptions />
                             </select>
                     </div>
                 </div>
